45abe75cb4e9e5f81a259d629bed6ace221e9be2
carmelitatalty <carmelitawarby@gmail.com>
2024-04-21 18:05:35 -0700

    Minor update

--- index.js
+++ index.js
@@ -428,7 +428,7 @@ app.get("/images", (req, res) => {
       res.send(listObjectsResponse);
     }).catch((e) => {
       console.log(e);
-      res.status(400).send(e)
+      res.status(400).send(e.message)
     });
 });
 
