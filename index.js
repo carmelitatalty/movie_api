8e75a64c11b94c78f38ea0c78af2b1ac5f5869a8
carmelitatalty <carmelitawarby@gmail.com>
2024-04-23 22:55:48 -0700

    File limit update

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const { v4: uuidv4 } = require("uuid");
 const BUCKET_NAME = '2-6-images';
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb", extended: true }));
+app.use(express.urlencoded({ limit: "10mb", extended: true, parameterLimit: 50000 }));
 const cors = require("cors");
 app.use(cors());
 // let allowedOrigins = ['2-6-frontend.s3-website-us-west-2.amazonaws.com'];
