fdc1d858c950a3838a48b9c8e75578a3d6238c1f
carmelitatalty <carmelitawarby@gmail.com>
2024-04-25 14:51:21 -0700

    Minor

--- testIt.js
+++ testIt.js
@@ -1,5 +1,6 @@
 
 const aws_s3 = require('./aws-s3.js')
+const fs = require("fs")
 
 // aws_s3.uploadImage('/tmp/dune.jpg', '2-6-images', 'dune-temp.jpg', null)
 const fileContent = fs.readFileSync(tempPath);
