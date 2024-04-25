b6bda4320bcb7dd2d0d2fba60ffb1760e5774889
carmelitatalty <carmelitawarby@gmail.com>
2024-04-25 16:11:49 -0700

    minor

--- testIt.js
+++ testIt.js
@@ -1,14 +1,18 @@
+const aws_s3 = require("./aws-s3.js");
+const fs = require("fs");
 
-const aws_s3 = require('./aws-s3.js')
-const fs = require("fs")
-
-const BUCKET_NAME = '2-6-images';
-const FILE_NAME = '/tmp/dune.jpg'
+const BUCKET_NAME = "2-6-images";
+const FILE_NAME = "/tmp/dune.jpg";
 // aws_s3.uploadImage('/tmp/dune.jpg', '2-6-images', 'dune-temp.jpg', null)
 const fileContent = fs.readFileSync(FILE_NAME);
-await aws_s3.put(fileContent, BUCKET_NAME, 'put-dune-temp.jpg', null)
+aws_s3.put(fileContent, BUCKET_NAME, "put-dune-temp.jpg", null).then(() => {
+  console.log(`Finished put`);
+});
 
-const result = await aws_s3.upload('upload-dune-temp.jpg', BUCKET_NAME, FILE_NAME)
-console.log(JSON.stringify(result))
+aws_s3
+  .upload("upload-dune-temp.jpg", BUCKET_NAME, FILE_NAME)
+  .then((result) => {
+    console.log(JSON.stringify(result));
+  });
 
-aws_s3.uploadImage(FILE_NAME, BUCKET_NAME, 'upload-image-dune-temp.jpg', null);
+aws_s3.uploadImage(FILE_NAME, BUCKET_NAME, "upload-image-dune-temp.jpg", null);
