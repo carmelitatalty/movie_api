3664d6267a2bd2a463607f8c755b0482e4f5d58e
carmelitatalty <carmelitawarby@gmail.com>
2024-04-25 14:51:50 -0700

    Minor

--- testIt.js
+++ testIt.js
@@ -3,5 +3,5 @@ const aws_s3 = require('./aws-s3.js')
 const fs = require("fs")
 
 // aws_s3.uploadImage('/tmp/dune.jpg', '2-6-images', 'dune-temp.jpg', null)
-const fileContent = fs.readFileSync(tempPath);
+const fileContent = fs.readFileSync('/tmp/dune.jpg');
 aws_s3.put(fileContent, '2-6-images', 'dune-temp.jpg', null)
