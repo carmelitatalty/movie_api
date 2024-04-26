c7aa02136f6dcf6b4f98d6d7f8eb79745f53f11c
carmelitatalty <carmelitawarby@gmail.com>
2024-04-25 19:28:48 -0700

    fix

--- testIt.js
+++ testIt.js
@@ -1,7 +1,6 @@
 const aws_s3 = require("./aws-s3.js");
 const fs = require("fs");
 
-
 // import { readFile } from "node:fs/promises";
 // const readFile = require("node:fs/promises")
 
@@ -12,66 +11,74 @@ const fileContent = fs.readFileSync(FILE_NAME);
 // aws_s3.put(fileContent, BUCKET_NAME, "put-dune-temp.jpg", null).then(() => {
 //   console.log(`Finished put`);
 // });
-const USERNAME = 'user20';
-const PASSWORD = 'password';
-const EMAIL = 'email@email.com'
+const USERNAME = "user20";
+const PASSWORD = "password";
+const EMAIL = "email@email.com";
 
-const createUser = async() => {
+const createUser = async () => {
+  const customHeaders = {
+    "Content-Type": "application/json",
+  };
   const createUserResponse = await fetch(`http://localhost/api/users`, {
-    method: 'POST',
+    method: "POST",
+    headers: customHeaders,
     body: JSON.stringify({
       Username: USERNAME,
       Password: PASSWORD,
-      Email: EMAIL
-    })
-  })
+      Email: EMAIL,
+    }),
+  });
   if (createUserResponse.ok) {
-    console.log(await createUserResponse.json())
+    console.log(await createUserResponse.json());
   } else {
-    console.log(`Response not ok ${createUserResponse.status}`)
-    console.log(await createUserResponse.text())
+    console.log(`Response not ok ${createUserResponse.status}`);
+    console.log(await createUserResponse.text());
   }
-  
-}
+};
 
 const uploadViaService = async () => {
-    console.log('Uploading via service')
-    const loginResponse = await fetch("http://localhost/api/login", {method: "POST", body: {
-        Username: USERNAME,
-        Password: PASSWORD
-    }})
-    console.log(loginResponse)
-    const responseBody = await loginResponse.json();
-    console.log(JSON.stringify(responseBody))
-    
-    const token = responseBody.body.token;
-    
-    // const fileName = "./sample.txt";
-    const body = new FormData();
-    // const blob = new Blob([await fs.readFile(FILE_NAME)]);
-    const blob = new Blob([fileContent])
-    
-    body.set("image", blob, FILE_NAME);
-    
-    const resp = await fetch("http://localhost/api/images", {
-      method: "POST",
-      body,
-      headers: {
-        Authorization: 'Bearer ' + token,
-      }
-    });
-    
-    console.log(
-        "STATUS:",
-        resp.status,
-        "\nCONTENT TYPE:",
-        resp.headers.get("content-type"),
-      );
-      console.log("RAW BODY:", await resp.text());
-}
+  console.log("Uploading via service");
+  const loginResponse = await fetch("http://localhost/api/login", {
+    method: "POST",
+    body: {
+      Username: USERNAME,
+      Password: PASSWORD,
+    },
+  });
+  console.log(loginResponse);
+  const responseBody = await loginResponse.json();
+  console.log(JSON.stringify(responseBody));
+
+  const token = responseBody.body.token;
+
+  // const fileName = "./sample.txt";
+  const body = new FormData();
+  // const blob = new Blob([await fs.readFile(FILE_NAME)]);
+  const blob = new Blob([fileContent]);
+
+  body.set("image", blob, FILE_NAME);
+
+  const resp = await fetch("http://localhost/api/images", {
+    method: "POST",
+    body,
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
+
+  console.log(
+    "STATUS:",
+    resp.status,
+    "\nCONTENT TYPE:",
+    resp.headers.get("content-type")
+  );
+  console.log("RAW BODY:", await resp.text());
+};
 // await uploadViaService()
 // console.log("Done uploading via service")
-createUser().then(() => {uploadViaService().then(() => console.log("Done uploading via service"))})
+createUser().then(() => {
+  uploadViaService().then(() => console.log("Done uploading via service"));
+});
 // aws_s3
 //   .upload("upload-dune-temp.jpg", BUCKET_NAME, FILE_NAME)
 //   .then((result) => {
